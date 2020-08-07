978e9b93b335234eb35a851dca09e29147c27fb4
samson wong <steelerwong2005@hotmail.com>
2020-08-07 02:56:51 -0400

    updated back connection.js for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,24 @@
 var mysql = require("mysql");
 
-// var connection;
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection ({
-//         host:'localhost',
-//         user:'root',
-//         password:'hacktheplanet',
-//         database:'todoagain_db'
-//     })
-// }
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection ({
+        host:'localhost',
+        user:'root',
+        password:'hacktheplanet',
+        database:'todoagain_db'
+    })
+}
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user:"root",
-    password:"password",
-    database: "burger_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user:"root",
+//     password:"password",
+//     database: "burger_db"
+// });
 
 
 // Make Connection
